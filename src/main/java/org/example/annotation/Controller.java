9160f8a848ed9b9c715ad00dad939029fd40c43e
Leticia.k <87592790+LeticiaKang@users.noreply.github.com>
2023-11-07 23:46:54 +0900

    di-pratice

--- src/main/java/org/example/annotation/Controller.java
+++ src/main/java/org/example/annotation/Controller.java
@@ -5,7 +5,7 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-@Target({ElementType.TYPE}) //class, interface, enum declaration에 붙을 수 있는 annotation임
-@Retention(RetentionPolicy.RUNTIME) //유지기간 : runtime동안
+@Target({ElementType.TYPE}) //class, interface, enum declaration에 붙을 수 있는 annotation이다
+@Retention(RetentionPolicy.RUNTIME) //유지기간 : runtime 동안
 public @interface Controller {
 }
